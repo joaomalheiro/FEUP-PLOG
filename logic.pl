3a71f640aa904ef2ef1b111d9b52d4d7a6c0cf18
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2018-11-09 21:51:06 +0000

    trying to paint this shit, está mal ainda

--- logic.pl
+++ logic.pl
@@ -16,12 +16,8 @@ start :-
     gameState(X),
     displayGame(X),
     [H|T] = X,
-    getPiece(H,0,1,Y),
-    write(Y),
-    betweenBoard(2,1),
-    nl,
-    write('it went through').
-    
+    validPlay(X,1,0,0,1).
+        
     % H is the table, T is the player %,
     % verifyPlaysTable(H).
 
@@ -37,9 +33,37 @@ verifyPlaysList([C|L]):-
     verifyPlaysList(L).
 
 verifyPlaysCell(X):-
-    % verify possible playes for this cell %
+    % verify possible plays for this cell %
     write('. ').
 
+validKill(State,FromX,FromY, ToX, ToY):-
+    %checkIfFinished(FromX,FromY,ToX,ToY),
+    DirX is sign(ToX - FromX),
+    DirY is sign(ToY - FromY),
+    X2 is FromX+DirX,
+    Y2 is FromY+DirY.
+    %validKill(State, X2, Y2, ToX, ToY).
+
+
+checkPieces(State,FromX,FromY,ToX, ToY):-
+    [B|P] = State,
+    ((
+        P =:= 1 -> (getPiece(B,ToX, ToY, DestinyPiece) , DestinyPiece == 2, getPiece(B, FromX, FromY, PlayerPiece) , PlayerPiece == 3)
+    );
+    (
+        P =:= 2 -> (getPiece(B,ToX, ToY, DestinyPiece) , DestinyPiece == 3, getPiece(B, FromX, FromY, PlayerPiece) , PlayerPiece == 2)
+    )).
+
+checkIfFinished(FromX,FromY,ToX,ToY):-
+    not(FromX==ToX);
+    not(FromY==ToY).
+
+validPlay(State,FromX, FromY, ToX, ToY):-
+    betweenBoard(FromX,FromY),
+    betweenBoard(ToX,ToY),
+    checkPieces(State,FromX,FromY, ToX, ToY),
+    validKill(State,FromX,FromY, ToX, ToY),
+    nl.
 
 getPiece(Board, Row, Column, Value):-
     nth0(Row, Board, HelpRow),
