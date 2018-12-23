c5fe167e22a52d0afaf933ffba3544e22bd20522
João Malheiro <jonas_malheiro_sousa@hotmail.com>
2018-12-23 17:57:21 +0000

    Fixed

--- Second Project/src/hints.pl
+++ Second Project/src/hints.pl
@@ -12,15 +12,6 @@ generate_hints(Board, LeftHints, UpHints, RightHints, DownHints, N):-
         reverse_list_of_lists(TransposedBoard, [], TRBoard),
         generate_hints_aux(TRBoard , DownHints, [], N).
 
-generate_hints(Board, LeftHints, UpHints, RightHints, DownHints, N):-
-        generate_hints_aux(Board, LeftHints, [], N),
-        transpose(Board, TransposedBoard),
-        generate_hints_aux(TransposedBoard, UpHints, [], N),
-        reverse_list_of_lists(Board, [], ReversedBoard),
-        generate_hints_aux(ReversedBoard, RightHints, [], N),
-        reverse_list_of_lists(TransposedBoard, [], TRBoard),
-        generate_hints_aux(TRBoard , DownHints, [], N).
-
     reverse_list_of_lists([], ReversedBoard, ReversedBoard).
     reverse_list_of_lists([H|T], ReversedAux, ReversedBoard):-
         reverse(H, ReversedH),
@@ -35,11 +26,10 @@ generate_hints(Board, LeftHints, UpHints, RightHints, DownHints, N):-
         generate_hints_aux(T, Hints, HintsAux2, N).
 
     generate_hints_list(_, List, List, N, N).
-    generate_hints_list(H, _, _, Index, N):-
+    generate_hints_list(H, List, ListAux, Index, N):-
         generate_random_num(1,12,1),
         Index2 is Index + 1,
-        generate_hints_list(H, _,_, Index2, N).
-
+        generate_hints_list(H, List, ListAux, Index2, N).
     generate_hints_list(H, List, ListAux, Index, N):-
         nth0(Index, H, Elem),
         append(ListAux, [Elem], ListAux2),
